90291e3c5277e6dc9811bf2bf78c290174b91166
Pedro José Anula <pedrojanula@gmail.com>
2017-04-22 00:13:03 +0200

    Añadido juego que genera un número aleatorio y el usuario debe adivinarlo.

---
+++ Adivina/Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="Default.aspx.vb" Inherits="_Default" Debug="true" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Adivina el número</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:TextBox ID="TBnum" MaxLength="3" placeholder="Introduce un número del 0 al 99: " runat="server"></asp:TextBox>
+        <asp:Label ID="LBsecret" runat="server" Text="0" Visible="false"></asp:Label>
+        <asp:Label ID="cont" runat="server" Text="0" Visible="false"></asp:Label>
+        <asp:Label ID="LBfin" Visible="false" runat="server">¡Correcto!</asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Adivina/Default.aspx.vb
@@ -0,0 +1,26 @@
+﻿
+Partial Class _Default
+    Inherits System.Web.UI.Page
+
+    Private Sub _Default_Load(sender As Object, e As EventArgs) Handles Me.Load
+        If LBsecret.Text = 0 Then
+            Dim rnd = New Random()
+            LBsecret.Text = rnd.Next(1, 100)
+            cont.Text = 0
+        End If
+
+        If IsPostBack Then
+            cont.Text = cont.Text + 1
+            LBfin.Visible = True
+
+            If LBsecret.Text > TBnum.Text Then
+                LBfin.Text = "El número buscado es mayor." + LBsecret.Text
+            ElseIf LBsecret.Text < TBnum.Text Then
+                LBfin.Text = "El número buscado es menor." + LBsecret.Text
+            Else
+                LBfin.Text = "¡CORRECTO! - Número de intentos: " + cont.Text
+                TBnum.Visible = False
+            End If
+        End If
+    End Sub
+End Class
