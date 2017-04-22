8b583b372d1174ec6a3bf5a6e5dda1c63da5cbab
Pedro José Anula <pedrojanula@gmail.com>
2017-04-23 00:05:47 +0200

    Añadida aplicación que comprueba si un DNI es válido o no.

---
+++ DNI/Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="VB" AutoEventWireup="true" CodeFile="Default.aspx.vb" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Comprobar DNI</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Panel ID="PN1" runat="server">
+            <asp:TextBox ID="TBDNI" runat="server" placeholder="Introduce DNI: " Columns="10" MaxLength="9" AutoPostBack="true"></asp:TextBox>
+        </asp:Panel>
+        <br />
+        <asp:Label ID="LBMensaje" Text="" runat="server"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ DNI/Default.aspx.vb
@@ -0,0 +1,34 @@
+﻿
+Partial Class _Default
+    Inherits System.Web.UI.Page
+
+    Private Shared chars As String = "TRWAGMYFPDXBNJZSQVHLCKE"
+
+    Private Sub TBDNI_TextChanged(sender As Object, e As EventArgs) Handles TBDNI.TextChanged
+        If Comprobar(TBDNI.Text) Then
+            LBMensaje.Text = "Correcto"
+        Else
+            LBMensaje.Text = "Incorrecto"
+        End If
+    End Sub
+
+    Public Function Comprobar(ByVal num As String) As Boolean
+        Dim l = num(num.Length - 1)
+        Dim n = num.Substring(0, num.Length - 1)
+        Dim response As Boolean = Comprobar(n, l)
+        Return response
+    End Function
+
+    Public Function Comprobar(ByVal num As String, ByVal c As Char) As Boolean
+        Dim response = False
+        Try
+            c = Char.ToUpper(c)
+            Dim Le As Char = chars(Int64.Parse(num) Mod 23)
+            If (Le = c) Then response = True
+        Catch ex As Exception
+
+        End Try
+
+        Return response
+    End Function
+End Class
